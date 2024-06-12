520dc3baea7ce33422abbd9d1a293661a3d7ef37
winbo <nowosiad6111+winbo@gmail.com>
2024-06-12 20:19:26 +0200

    Add clear commands

--- Downgrader.bat
+++ Downgrader.bat
@@ -6,26 +6,32 @@ title InsiderTo11Downgrader
 echo This tool will downgrade your Windows 11 Insider version to the Windows 11 Stable version.
 echo Press any key to begin the process.
 pause>nul
+cls
 
 color 47
 echo WARNING - THIS MAY BREAK YOUR SYSTEM. MAKE SURE TO HAVE A WINDOWS USB ON HAND AND A BACKUP OF ALL YOUR DATA. I AM NOT RESPONSIBLE FOR ANY DAMAGES CAUSED BY THIS TOOL.
 echo Press any key if you understand what you're doing.
 pause>nul
+cls
 
 set /p download=Please paste in the download URL for your Windows 11 ISO. To do this, right click on the Download button on the site and copy the link: 
 echo Downloading the Windows 11 ISO. Please wait patiently.
 curl %download% -o %temp%\Windows.iso>nul
+cls
 
 %temp%\Windows.iso
 set /p %DRIVEPOINT%=Please check the path the downloaded Windows ISO has been mounted to (example: F:\): 
+cls
 
 echo Copying installation files to C:\winISO. Please wait...
 md C:\winISO
 xcopy %DRIVEPOINT% C:\winISO\ /S /E /I /Y
+cls
 
 echo Complete. Now patching ISO.
 del C:\winISO\sources\setupcompat.dll
 copy setupcompat.dll C:\winISO\sources\
+cls
 
 echo Complete. Now starting the installer and exiting (since it's up to you on using the installer now).
 echo Press any key to exit...
